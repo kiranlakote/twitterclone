209cf2d52b1338e7fe63ec4972d790946b76ab83
kiranlakote <lakotekiran@gmail.com>
2018-05-25 15:50:17 +0530

    complete

---
+++ db/migrate/20180525085631_add_fields_to_users.rb
@@ -0,0 +1,7 @@
+class AddFieldsToUsers < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :users, :name, :strings
+  	add_column :users, :username, :string
+  	add_index :users, :username, unique: true
+  end
+end
