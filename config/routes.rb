e79c983b06c814d6e44ae220342c30619b8f45e9
kiranlakote <lakotekiran@gmail.com>
2018-05-26 17:33:03 +0530

    like and dislike

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :tweeets do
   	resources :comments
+  	member do
+  		put 'like', to: "tweeets#like"
+  		put 'unlike', to: "tweeets#unlike"
+  	end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tweeets#index"
